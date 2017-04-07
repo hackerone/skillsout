a7d4189678460f5879efc3c4fd4ef83d25d0ebf0
parth.mahida <parth.mahida@infizer.com>
2017-04-07 17:35:16 +0530

    HOme page design of footer and main-content

--- client/main/main.controller.ng.js
+++ client/main/main.controller.ng.js
@@ -1,51 +1,81 @@
 'use strict'
+angular.module('skillsoutApp').controller('MainCtrl', function($scope) {
+    $scope.page = 1;
+    $scope.perPage = 3;
+    $scope.sort = {
+        name_sort: 1
+    };
+    $scope.orderProperty = '1';
+    $scope.teachersProfile = [{
+        name: 'Parth',
+        start: 5,
+        subject: 'Salsa',
+        profileImg:'img/porofile_img.jpg',
+        _id:1
+    }, {
+        name: 'Parth',
+        start: 5,
+        subject: 'Salsa',
+         profileImg:'img/porofile_img.jpg',
+        _id:2
+    }, {
+        name: 'Parth',
+        start: 5,
+        subject: 'Salsa',
+         profileImg:'img/porofile_img.jpg',
+        _id:3
+    }, {
+        name: 'Parth',
+        start: 5,
+        subject: 'Salsa',
+         profileImg:'img/porofile_img.jpg',
+        _id:4
+    }, {
+        name: 'Parth',
+        start: 5,
+        subject: 'Salsa',
+         profileImg:'img/porofile_img.jpg',
+        _id:5
+    }];
 
-angular.module('skillsoutApp')
-.controller('MainCtrl', function($scope) {
-  $scope.page = 1;
-  $scope.perPage = 3;
-  $scope.sort = {name_sort : 1};
-  $scope.orderProperty = '1';
-  
-  $scope.helpers({
-    things: function() {
-      return Things.find({}, {
-        sort: $scope.getReactively('sort') 
-      });
-    },
-    thingsCount: function() {
-      return Counts.get('numberOfThings');
-    }
-  });
-                  
-  $scope.subscribe('things', function() {
-    return [{
-      sort: $scope.getReactively('sort'),
-      limit: parseInt($scope.getReactively('perPage')),
-      skip: ((parseInt($scope.getReactively('page'))) - 1) * (parseInt($scope.getReactively('perPage')))
-    }, $scope.getReactively('search')];
-  });
 
-  $scope.save = function() {
-    if ($scope.form.$valid) {
-      Things.insert($scope.newThing);
-      $scope.newThing = undefined;
-    }
-  };
-                  
-  $scope.remove = function(thing) {
-    Things.remove({_id: thing._id});
-  };
-                  
-  $scope.pageChanged = function(newPage) {
-    $scope.page = newPage;
-  };
-                  
-  return $scope.$watch('orderProperty', function() {
-    if ($scope.orderProperty) {
-      $scope.sort = {
-        name_sort: parseInt($scope.orderProperty)
-      };
-    }
-  });
+
+    $scope.helpers({
+        things: function() {
+            return Things.find({}, {
+                sort: $scope.getReactively('sort')
+            });
+        },
+        thingsCount: function() {
+            return Counts.get('numberOfThings');
+        }
+    });
+    $scope.subscribe('things', function() {
+        return [{
+            sort: $scope.getReactively('sort'),
+            limit: parseInt($scope.getReactively('perPage')),
+            skip: ((parseInt($scope.getReactively('page'))) - 1) * (parseInt($scope.getReactively('perPage')))
+        }, $scope.getReactively('search')];
+    });
+    $scope.save = function() {
+        if ($scope.form.$valid) {
+            Things.insert($scope.newThing);
+            $scope.newThing = undefined;
+        }
+    };
+    $scope.remove = function(thing) {
+        Things.remove({
+            _id: thing._id
+        });
+    };
+    $scope.pageChanged = function(newPage) {
+        $scope.page = newPage;
+    };
+    return $scope.$watch('orderProperty', function() {
+        if ($scope.orderProperty) {
+            $scope.sort = {
+                name_sort: parseInt($scope.orderProperty)
+            };
+        }
+    });
 });
